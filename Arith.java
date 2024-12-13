a38fdd58361bc4c0b7595b2e70953c58e2affa12
Meghana-46 <meghana.gal2002@gmail.com>
2024-12-13 16:03:22 +0530

    Arithmetic

---
+++ Arith.java
@@ -0,0 +1,11 @@
+class Arith {
+  public static void main(String[] args) {
+    
+    int a = -50, b =11;
+    System.out.println("a + b = " + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("a % b = " + (a % b));
+  }
+}
